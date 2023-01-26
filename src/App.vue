0c905c62d9c6491bbaee8cc5b09e97a42a47df8a
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-26 17:44:12 +0100

    PUSH MILESTONE 4

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,9 @@ export default {
     <div class="container">
         <h1>Elenco Progetti CM</h1>
         <div class="proj_box" v-for="project in projects" :key="project.id">
-          <h3 class="proj_title">{{project}}</h3>
+          <h2 class="proj_title">{{project.name}}</h2>
+          <p class="proj_title">{{project.summary}}</p>
+          <p class="proj_title">Category: {{project.summary}}</p>
         </div>
     </div>
   </div>
