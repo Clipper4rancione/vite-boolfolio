403073b0ca3b38c78e40808b5821c8723074729a
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-26 19:01:31 +0100

    [add] projectcard

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,36 @@
 <script>
-
 import axios from 'axios';
-
+import ProjectCard from './components/ProjectCard.vue';
 export default {
-  name: 'App',
-  data(){
-    return{
-      baseUrl: 'http://127.0.0.1:8000/api/'
-    }
-  },
-  methods:{
-    getApi(){
-        axios.get(this.baseUrl + 'projects')
+    name: "App",
+    components:{
+      ProjectCard
+    },
+    data(){
+        return{
+            baseUrl: 'http://127.0.0.1:8000/api/'
+        }
+    },
+    methods: {
+        getApi(){
+
+            axios.get(this.baseUrl + 'projects')
             .then(result => {
                 this.projects = result.data.project_array;
+                console.log(this.projects);
             })
-  
-
+            
+        }
     },
-
-  },
-  mounted(){
-    this.getApi();
-
-  }
+    mounted(){
+        this.getApi();
+        
+    }
 }
 </script>
 
 <template>
-  <div class="main-wrapper">
+    <div class="main-wrapper">
     <div class="container">
         <h1>Elenco Progetti CM</h1>
         <div class="proj_box" v-for="project in projects" :key="project.id">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,21 @@
+<script>
+
+
+
+export default {
+    name: "ProjectCard",
+    props:{
+        project: Object
+    }
+}
+</script>
+
+<template>
+    
+</template>
+
+
+<style lang="scss">
+
+  
+</style>
