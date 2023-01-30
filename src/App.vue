fdeb7055038f64d25dad35a16026a4a4196f4ff9
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-30 17:52:53 +0100

    [add] ProjectCard component with props

--- src/App.vue
+++ src/App.vue
@@ -36,9 +36,7 @@ export default {
     <div class="container">
         <h1>Elenco Progetti CM</h1>
         <div class="proj_box" v-for="project in projects" :key="project.id">
-          <h2 class="proj_title">{{project.name}}</h2>
-          <p class="proj_title">{{project.summary}}</p>
-          <p class="proj_title">Category: {{project.summary}}</p>
+          <ProjectCard :project="project"/>
         </div>
     </div>
   </div>
