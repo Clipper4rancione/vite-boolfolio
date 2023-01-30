45e5510622c7a3892c102de6ec85423c24d0fc06
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-30 17:53:14 +0100

    [add] ProjectCard component template

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -11,11 +11,13 @@ export default {
 </script>
 
 <template>
-    
+        <h2 class="proj_title">{{project.name}}</h2>
+        <p class="proj_title">{{project.summary}}</p>
+        <p class="proj_title">Category: {{project.summary}}</p>
 </template>
 
 
 <style lang="scss">
-
+@use '../style/general'
   
 </style>
